4a34f7b073f73910be783c28340feb97aa2b1d6b
Louis Tsai <louis993546@gmail.com>
2021-07-10 19:07:33 +0200

    Super hacky home screen grid

--- demo/src/main/java/com/louis993546/metro/MainActivity.kt
+++ demo/src/main/java/com/louis993546/metro/MainActivity.kt
@@ -1,31 +1,36 @@
 package com.louis993546.metro
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.louis993546.metro.ui.theme.MetroDemoTheme
+import timber.log.Timber
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MetroDemoTheme {
-                VerticalTilesGrid {
-                    Tile(title = "Tile 1")
-                    Tile(title = "Tile 2")
-                    Tile(title = "Tile 3")
-                    Tile(title = "Tile 4")
-                    Tile(title = "Tile 5")
+                VerticalTilesGrid2 {
+                    // TODO figure out how to do this abstract these
+                    Tile(title = "Tile 1").also { TilesGridScopeInstance.whatIsThisList.add(WhatIsThis(1, 1)) }
+                    Tile(title = "Tile 2").also { TilesGridScopeInstance.whatIsThisList.add(WhatIsThis(1, 1)) }
+                    Tile(title = "Tile 3").also { TilesGridScopeInstance.whatIsThisList.add(WhatIsThis(2, 2)) }
+                    Tile(title = "Tile 4").also { TilesGridScopeInstance.whatIsThisList.add(WhatIsThis(1, 1)) }
+                    Tile(title = "Tile 5").also { TilesGridScopeInstance.whatIsThisList.add(WhatIsThis(4, 2)) }
                 }
             }
         }
@@ -39,12 +44,8 @@ fun Tile(
     backgroundColor: Color = LocalAccentColor.current,
     textColor: Color = LocalTextOnAccentColor.current
 ) {
-    Log.d("qqqq", backgroundColor.toString())
     Box(
-        modifier = modifier
-            .background(color = backgroundColor)
-            .aspectRatio(1f)
-    ) {
+        modifier = modifier.background(color = backgroundColor)) {
         Text(
             modifier = Modifier.align(Alignment.BottomStart),
             text = title,
@@ -53,13 +54,180 @@ fun Tile(
     }
 }
 
+private fun Int.toColumnCount(): Int = if (this > 360) 6 else 4
+
+interface TilesGridScope {
+    fun s(content: @Composable () -> Unit)
+    fun m(content: @Composable () -> Unit)
+    fun l(content: @Composable () -> Unit)
+}
+
+class WhatIsThis(
+    val columnCount: Int,
+    val rowCount: Int,
+//    val content: (index: Int) -> @Composable () -> Unit
+)
+
+object TilesGridScopeInstance : TilesGridScope {
+    val whatIsThisList = mutableListOf<WhatIsThis>()
+
+    override fun s(content: @Composable () -> Unit) {
+        whatIsThisList.add(
+            WhatIsThis(
+                columnCount = 1,
+                rowCount = 1,
+//                content = { @Composable { content() } },
+            )
+        )
+    }
+
+    override fun m(content: () -> Unit) {
+        whatIsThisList.add(
+            WhatIsThis(
+                columnCount = 2,
+                rowCount = 2,
+//                content = { @Composable { content() } },
+            )
+        )
+    }
+
+    override fun l(content: () -> Unit) {
+        whatIsThisList.add(
+            WhatIsThis(
+                columnCount = 4,
+                rowCount = 2,
+//                content = { @Composable { content() } },
+            )
+        )
+    }
+
+}
+
 @Composable
-fun VerticalTilesGrid(
+fun VerticalTilesGrid2(
     modifier: Modifier = Modifier,
-    columns: Int = 2, // TODO make this four
+    columns: Int = LocalConfiguration.current.screenWidthDp.toColumnCount(),
+    gap: Dp = 8.dp,
     content: @Composable () -> Unit
 ) {
     Layout(
+        content = content,
+        modifier = modifier, // TODO make this scrollable
+    ) { measurables, constraints ->
+        val totalGapWidth = gap.roundToPx() * (columns + 1)
+        val cellSize = (constraints.maxWidth - totalGapWidth) / columns
+
+        val maxPossibleRowCount = measurables
+            .mapIndexed { index, _ -> TilesGridScopeInstance.whatIsThisList[index].rowCount }
+            .sum()
+        val matrix = (0 until maxPossibleRowCount).map {
+            ((0 until columns).map { -1 }).toMutableList()
+        }.toMutableList()
+
+        val placeablesWithCoordinates = measurables.mapIndexed { index, measurable ->
+            val whatIsThis = TilesGridScopeInstance.whatIsThisList[index]
+            val columnCount = whatIsThis.columnCount
+            val rowCount = whatIsThis.rowCount
+
+            var currentRow = 0
+            var currentColumn = 0
+
+            var row: Int? = null
+            var column: Int? = null
+
+            while (row == null || column == null) {
+                if (matrix[currentRow][currentColumn] != -1) {
+                    // this cell is already taken
+                    if (currentColumn == (columns - 1)) {
+                        currentRow += 1
+                        currentColumn = 0
+                    } else {
+                        currentColumn += 1
+                    }
+                } else {
+                    // this cell is empty, not check if all the necessary cells are empty as well
+                    // check if there is enough columns first
+                    if ((columns - currentColumn) < columnCount) {
+                        currentRow += 1
+                        currentColumn = 0
+                    } else {
+                        // For now, only needs to check cells same row on the right
+                        val cellsToCheck = matrix[currentRow].subList(currentColumn, currentColumn + columnCount)
+                        if (cellsToCheck.any { it != -1 }) {
+                            currentRow += 1
+                            currentColumn = 0
+                        } else {
+                            (currentRow until (currentRow + rowCount)).forEach { r ->
+                                (currentColumn until (currentColumn + columnCount)).forEach { c ->
+                                    Timber.tag("qqqq").d("[$r][$c]")
+                                    matrix[r][c] = index
+                                }
+                            }
+                            Timber.tag("qqqq").d("-----")
+                            row = currentRow
+                            column = currentColumn
+                        }
+                    }
+                }
+            }
+
+            val itemWidth = cellSize * columnCount + gap.roundToPx() * (columnCount - 1)
+            val itemHeight = itemWidth / columnCount * whatIsThis.rowCount
+
+            val itemConstraints = constraints.copy(
+                minWidth = itemWidth,
+                maxWidth = itemWidth,
+                minHeight = itemHeight,
+                maxHeight = itemHeight,
+            )
+
+            PlaceableWithGridCoordinate(
+                placeable = measurable.measure(itemConstraints),
+                row = row,
+                column = column,
+                heightRowCount = rowCount,
+            )
+        }
+
+        val totalRowCount = placeablesWithCoordinates.maxByOrNull { it.row }?.row ?: 0
+        val tallestLastRowRowCount = placeablesWithCoordinates.filter { it.row == totalRowCount }.maxByOrNull { it.heightRowCount }?.heightRowCount ?: 1
+        val totalHeight = ((totalRowCount + 1) * cellSize) + ((totalRowCount + 2) * gap.roundToPx()) + ((tallestLastRowRowCount - 1) * cellSize)
+
+        layout(
+            width = constraints.maxWidth,
+            height = totalHeight,
+        ) {
+            placeablesWithCoordinates.forEach { (placeable, row, column) ->
+                val x = ((column + 1) * gap.roundToPx()) + (column * cellSize)
+                val y = ((row + 1) * gap.roundToPx()) + (row * cellSize)
+
+                Timber.tag("qqqq").d("x = $x, y = $y")
+
+                placeable.placeRelative(
+                    x = x,
+                    y = y,
+                )
+            }
+        }
+    }
+}
+
+data class PlaceableWithGridCoordinate(
+    val placeable: Placeable,
+    val row: Int,
+    val column: Int,
+    val heightRowCount: Int,
+)
+
+@Composable
+fun VerticalTilesGrid(
+    modifier: Modifier = Modifier,
+    columns: Int = LocalConfiguration.current.screenWidthDp.toColumnCount(),
+    gap: Dp = 8.dp,
+    content: @Composable () -> Unit
+) {
+    Layout(
+//        content = { TilesGridScopeInstance.content() },
         content = content,
         modifier = modifier
     ) { measurables, constraints ->
@@ -126,4 +294,7 @@ fun DefaultPreview() {
 //    MetroDemoTheme {
 //        Greeting("Android")
 //    }
+    LazyColumn() {
+        
+    }
 }
