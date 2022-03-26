caabd6f24173013ba5a99ec50a3039308ebf30dc
Louis <louis993546@gmail.com>
2022-03-26 01:41:55 +0100

    15 percent of app search & TextField

--- app_search/src/main/java/com/louis993546/app_search/AppSearch.kt
+++ app_search/src/main/java/com/louis993546/app_search/AppSearch.kt
@@ -1,22 +1,46 @@
 package com.louis993546.app_search
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.louis993546.app_row.AppRow
 import com.louis993546.apps.Apps
+import com.louis993546.metro.TextField
 
 @Composable
 fun AppSearch(
     modifier: Modifier = Modifier,
     apps: List<Apps>,
 ) {
-    LazyColumn(
-        modifier = modifier
+    var searchQuery by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier,
     ) {
-        items(apps, { it.id }) {
-            AppRow(appName = it.name)
+        TextField(
+            modifier = Modifier.padding(8.dp).fillMaxWidth(),
+            value = searchQuery,
+            onValueChange = { searchQuery = it },
+        )
+        LazyColumn(
+            modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
+//            contentPadding = PaddingValues(top = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(apps, { it.id }) {
+                AppRow(appName = it.name)
+            }
         }
     }
 }
---
+++ metro/src/main/java/com/louis993546/metro/TextField.kt
@@ -0,0 +1,27 @@
+package com.louis993546.metro
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .heightIn(min = 36.dp)
+            .background(color = Color.White)
+            .border(2.dp, color = LocalAccentColor.current)
+    ) {
+        BasicTextField(value = value, onValueChange = onValueChange)
+    }
+}
