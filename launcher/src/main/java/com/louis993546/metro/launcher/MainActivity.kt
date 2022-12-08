2911339092b43e601484502562332623e8c359ca
Louis <louis993546@gmail.com>
2022-12-08 23:53:26 +0100

    Able to get list of apps to show up

--- launcher/src/main/java/com/louis993546/metro/launcher/MainActivity.kt
+++ launcher/src/main/java/com/louis993546/metro/launcher/MainActivity.kt
@@ -1,22 +1,37 @@
 package com.louis993546.metro.launcher
 
+import android.content.pm.LauncherApps
 import android.os.Bundle
+import android.os.Process
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.louis993546.metro.MetroTheme
 import com.louis993546.metro.Text
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val installedApps = getInstalledApps()
+
         setContent {
             MetroLauncherTheme {
-                Text(text = "Hello")
+                Column {
+                    installedApps.forEach { app ->
+                        Text(text = app)
+                    }
+                }
             }
         }
     }
+
+    private fun getInstalledApps(): List<String> = getSystemService(LauncherApps::class.java)
+        .getActivityList(null, Process.myUserHandle())
+        .map { it.label.toString() }
+        .sorted()
 }
 
 @Composable
