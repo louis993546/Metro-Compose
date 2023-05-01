ed7ef4c7d5943a4c0b4390178e445e5c2459e6ff
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2023-05-01 23:39:51 +0200

    demoAppSearch: Refactor to take advantage of `ListView`.

--- demoAppSearch/src/main/java/com/louis993546/metro/demo/appSearch/AppSearch.kt
+++ demoAppSearch/src/main/java/com/louis993546/metro/demo/appSearch/AppSearch.kt
@@ -1,7 +1,10 @@
 package com.louis993546.metro.demo.appSearch
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,7 +18,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.louis993546.metro.ListView
+import com.louis993546.metro.ListViewHeaderAcronymStyle
+import com.louis993546.metro.ListViewItem
+import com.louis993546.metro.LocalBackgroundColor
 import com.louis993546.metro.TextField
 import com.louis993546.metro.demo.appRow.AppRow
 import com.louis993546.metro.demo.apps.Apps
@@ -25,36 +33,49 @@ fun AppSearch(
     modifier: Modifier = Modifier,
     apps: List<Apps>,
 ) {
-    var searchQuery by remember { mutableStateOf("") }
-    val focusRequester = remember { FocusRequester() }
+    Column(modifier = modifier) {
+        var searchQuery by remember { mutableStateOf("") }
+        val focusRequester = remember { FocusRequester() }
 
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 12.dp),
-    ) {
-        TextField(
+        val list = apps
+            .sorted()
+            .map { app ->
+                ListViewItem.Content(
+                    contents = { AppRow(name = app.id, icon = null) },
+                    label = app.id,
+                    key = app.id
+                )
+            }
+
+        Column(
             modifier = Modifier
-                .focusRequester(focusRequester)
-                .padding(8.dp)
-                .padding(bottom = 32.dp)
-                .fillMaxWidth(),
-            value = searchQuery,
-            onValueChange = { searchQuery = it },
-        )
-        LazyColumn(
-            modifier = Modifier
-                .weight(1f)
-                .padding(horizontal = 8.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
+                .fillMaxSize()
+                .padding(vertical = 6.dp)
+                .padding(horizontal = 22.dp),
         ) {
-            items(apps, { it.id }) {
-                AppRow(name = it.name, icon = null)
+            TextField(
+                modifier = Modifier
+                    .focusRequester(focusRequester)
+                    .fillMaxWidth()
+                    .padding(bottom = 12.dp),
+                value = searchQuery,
+                onValueChange = { searchQuery = it },
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 4.dp)
+            ) {
+                ListView(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    items = list,
+                )
             }
         }
-    }
 
-    LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
     }
 }
